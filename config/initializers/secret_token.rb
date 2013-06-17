452b94ea33895c2dca0144f9b011c81b8390e147
kai.manaka <kai.manaka@hamee.co.jp>
2013-06-17 13:02:34 +0900

    Railsアプリケーションの作成と必要なライブラリのインストール済み
    RSpec、TwitterBootstrapのファイル生成は行っていない状態

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nsn::Application.config.secret_token = '98d11edbf789a25db3e7ef31a47d3cd95b5ce4482cf1c73c9ada6f24918d28f8f399cf9796cb51e3ee7fccde7be880f3cca1d4128b5a1c3358728b5ea42f2de2'
