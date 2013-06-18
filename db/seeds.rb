c74f7158e1dc65ecf82159b0321058bd23c585e1
namachapanda <manaka-20.lisardggy@apost.plala.or.jp>
2013-06-18 19:01:04 +0900

    categoriesテーブルとcolorsテーブルの作成及びデータ格納

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Category.create(:id => 1, :name => 'キク科')
+# Category.create(:id => 2, :name => 'バラ科')
+# Category.create(:id => 3, :name => 'アサガオ科')
+
+Color.create(:id => 1, :name => '赤')
+Color.create(:id => 2, :name => '青')
+Color.create(:id => 3, :name => '黄')
